d11981be6b065eb5fd850d20490ea5c8008f3f1b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-17 10:52:56 -0700

    update webpage to allow user to enter in animal details

---
+++ Shelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace Shelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string AnimalType { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string DateOfAdmit { get; set; }
+    public string Breed { get; set; }
+  }
+}
